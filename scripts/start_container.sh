9def6afdb61dc61a994908fa42314c7e32baf5f9
fuyyzet <116317525+fuyyzet@users.noreply.github.com>
2023-12-07 23:03:56 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
  
 # Pull the Docker image from Docker Hub
 docker pull fuzzyet/simple-python-flask-app
-
+   
 # Run the Docker image as a container
 docker run -d -p 5000:5000 fuzzyet/simple-python-flask-app
